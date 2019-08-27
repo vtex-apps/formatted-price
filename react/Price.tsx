a2a473b80c865380a9eb38c5f94b20d93565cc40
Jefferson <jefiim.ad@gmail.com>
2019-08-27 19:52:52 -0300

    setup

---
+++ react/Price.tsx
@@ -0,0 +1,33 @@
+import React, { FC, Fragment } from 'react'
+import {
+  defineMessages,
+  injectIntl,
+  InjectedIntlProps,
+  FormattedMessage,
+} from 'react-intl'
+import { FormattedCurrency } from 'vtex.format-currency'
+
+defineMessages({
+  Free: {
+    id: 'store/price.Free',
+    defaultMessage: 'FREE',
+  },
+})
+
+const Price: FC<PriceProps & InjectedIntlProps> = ({ value }) => {
+  return (
+    <Fragment>
+      {value === 0 ? (
+        <FormattedMessage id="store/price.Free" />
+      ) : (
+        <FormattedCurrency value={value} />
+      )}
+    </Fragment>
+  )
+}
+
+interface PriceProps {
+  value: number
+}
+
+export default injectIntl(Price)
